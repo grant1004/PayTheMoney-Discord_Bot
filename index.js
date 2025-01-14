89c323048501222e63ae494d417edfb9a04c6931
grant1004 <grant891004@gmail.com>
2025-01-14 23:40:09 +0800

    Update '借錢的腦殘'

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('interactionCreate', async interaction => {
         // 債務人姓名輸入
         const debtorInput = new TextInputBuilder()
             .setCustomId('debtorName')
-            .setLabel('該付錢的人名')
+            .setLabel('借錢的腦殘')
             .setStyle(TextInputStyle.Short)
             .setRequired(true);
 
